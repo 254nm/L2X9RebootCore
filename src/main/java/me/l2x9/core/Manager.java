bc90959709f8442282e879012a29b099320092e1
Nate Steven Legault <67813185+254nm@users.noreply.github.com>
2022-04-02 21:46:46 -0400

    Add a method to get the module's data folder

--- src/main/java/me/l2x9/core/Manager.java
+++ src/main/java/me/l2x9/core/Manager.java
@@ -1,23 +1,30 @@
 package me.l2x9.core;
 
-import me.l2x9.core.boiler.util.ConfigCreator;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.configuration.ConfigurationSection;
 
+import java.io.File;
+
+@RequiredArgsConstructor
+@Getter
 public abstract class Manager {
 
+    @NonNull
     private final String name;
 
-    public Manager(String name) {
-        this.name = name;
-
-    }
+    protected File dataFolder;
 
     public abstract void init(L2X9RebootCore plugin);
 
     public abstract void destruct(L2X9RebootCore plugin);
 
-    public abstract void reloadConfig(ConfigCreator creator);
+    public abstract void reloadConfig(ConfigurationSection config);
 
-    public String getName() {
-        return name;
+    public File getDataFolder() {
+        if (dataFolder == null) dataFolder = new File(L2X9RebootCore.getInstance().getDataFolder(), getName());
+        if (!dataFolder.exists()) dataFolder.mkdirs();
+        return dataFolder;
     }
 }
