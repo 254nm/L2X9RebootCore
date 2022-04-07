5e29be04c052ae9657138b9f74c100ab5ed4255d
Nate Steven Legault <67813185+254nm@users.noreply.github.com>
2022-04-06 22:56:36 -0400

    Add player names to logging

--- src/main/java/me/l2x9/core/impl/patches/listeners/packetsize/compress/CustomPacketDecompressor.java
+++ src/main/java/me/l2x9/core/impl/patches/listeners/packetsize/compress/CustomPacketDecompressor.java
@@ -7,6 +7,7 @@ import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.DecoderException;
 import me.l2x9.core.util.Utils;
 import net.minecraft.server.v1_12_R1.PacketDataSerializer;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 import java.util.zip.Inflater;
@@ -14,10 +15,12 @@ import java.util.zip.Inflater;
 public class CustomPacketDecompressor extends ByteToMessageDecoder {
     private final Inflater inflater;
     private int threshold;
+    private final Player player;
 
-    public CustomPacketDecompressor(int threshold) {
+    public CustomPacketDecompressor(int threshold, Player player) {
         this.threshold = threshold;
         this.inflater = new Inflater();
+        this.player = player;
     }
 
     @Override
@@ -27,7 +30,7 @@ public class CustomPacketDecompressor extends ByteToMessageDecoder {
             int totalLength = pds.g(); //Read the first VarInt which should be length of Data length + compressed length of (Packet ID + Data)
             if (totalLength >= 1927951) {
                 pds.clear();
-                Utils.log(String.format("&aPrevented an extremely large packet with length of&r&3 %d/%d &r&afrom being received&r", totalLength, 1927951));
+                Utils.log(String.format("&aPrevented a large packet with length of&r&3 %d/%d &r&afrom player %s near %s&r", totalLength, 1927951, player.getName(), Utils.formatLocation(player.getLocation())));
                 return;
             }
             if (totalLength == 0) {
--- src/main/java/me/l2x9/core/impl/patches/listeners/packetsize/encode/CustomPacketEncoder.java
+++ src/main/java/me/l2x9/core/impl/patches/listeners/packetsize/encode/CustomPacketEncoder.java
@@ -3,36 +3,35 @@ package me.l2x9.core.impl.patches.listeners.packetsize.encode;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
+import lombok.RequiredArgsConstructor;
 import me.l2x9.core.util.Utils;
 import net.minecraft.server.v1_12_R1.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bukkit.entity.Player;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 public class CustomPacketEncoder extends MessageToByteEncoder<Packet<?>> {
-    private static final Logger logger = LogManager.getLogger();
-    private final EnumProtocolDirection direction;
-    private final int MAX_LEN = 2097152;
-
-    public CustomPacketEncoder(EnumProtocolDirection var1) {
-        this.direction = var1;
-    }
+    private final Logger logger = LogManager.getLogger();
+    private final Player player;
 
     @Override
     protected void encode(ChannelHandlerContext context, Packet<?> packet, ByteBuf buf) throws Exception {
         EnumProtocol protocol = context.channel().attr(NetworkManager.c).get();
         if (protocol == null) throw new RuntimeException("ConnectionProtocol unknown: " + packet.toString());
-        Integer id = protocol.a(direction, packet);
+        Integer id = protocol.a(EnumProtocolDirection.CLIENTBOUND, packet);
         if (id == null) throw new IOException("Can't serialize unregistered packet");
         PacketDataSerializer dataSerializer = new PacketDataSerializer(buf);
         dataSerializer.d(id);
         try {
             packet.b(dataSerializer);
             int len = dataSerializer.readableBytes();
-            if (len >= MAX_LEN) {
+            if (len >= 2097152) {
                 dataSerializer.clear();
-                Utils.log(String.format("&aPrevented an extremely large&r&3 %s &r&apacket with length of&r&3 %d/%d &r&afrom being sent&r", packet.getClass().getName(), len, MAX_LEN));
+                String longPacketFormat = "&aPrevented a large&r&3 %s &r&apacket with length of&r&3 %d/%d &r&afrom being sent to player&r&3 %s&r&a near &r&a%s&r";
+                Utils.log(String.format(longPacketFormat, packet.getClass().getSimpleName(), len, 2097152, player.getName(), Utils.formatLocation(player.getLocation())));
             }
         } catch (Throwable throwable) {
             logger.error(throwable);
