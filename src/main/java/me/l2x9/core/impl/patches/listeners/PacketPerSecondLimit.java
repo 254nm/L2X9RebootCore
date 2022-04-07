5b786a81c122e8313d1fd21f4e9a92d64b6bba31
Nate Steven Legault <67813185+254nm@users.noreply.github.com>
2022-04-06 22:57:15 -0400

    Use the hash of the uuid instead of the player

--- src/main/java/me/l2x9/core/impl/patches/listeners/PacketPerSecondLimit.java
+++ src/main/java/me/l2x9/core/impl/patches/listeners/PacketPerSecondLimit.java
@@ -4,6 +4,10 @@ import me.l2x9.core.ViolationManager;
 import me.l2x9.core.packet.PacketEvent;
 import me.l2x9.core.packet.PacketListener;
 import me.l2x9.core.util.Utils;
+import net.minecraft.server.v1_12_R1.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class PacketPerSecondLimit extends ViolationManager implements PacketListener {
     private final int PP_LIMIT = 225;
@@ -14,16 +18,15 @@ public class PacketPerSecondLimit extends ViolationManager implements PacketList
 
     @Override
     public void incoming(PacketEvent.Incoming event) throws Throwable {
-        increment(event.getPlayer().hashCode());
-        int vls = getVLS(event.getPlayer().hashCode());
+        increment(event.getPlayer().getUniqueId().hashCode());
+        int vls = getVLS(event.getPlayer().getUniqueId().hashCode());
         if (vls > PP_LIMIT) {
-            remove(event.getPlayer().hashCode());
+            remove(event.getPlayer().getUniqueId().hashCode());
             Utils.run(() -> Utils.kick(event.getPlayer(), "&cYou are sending packets too fast!"));
         }
     }
 
     @Override
     public void outgoing(PacketEvent.Outgoing event) throws Throwable {
-
     }
 }
