62e86a0e34a884077686659b0bfef92416819049
Nate Steven Legault <67813185+254nm@users.noreply.github.com>
2022-05-25 21:50:02 -0400

    ProtocolLib 🤮

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,16 +5,18 @@ plugins {
 
 repositories {
     mavenLocal()
+    maven { url = uri("https://repo.dmulloy2.net/repository/public/") } //This is only so I can patch chunkban on servers that use protocol lib
     maven { url = uri("https://repo.txmc.me/releases") }
     maven { url = uri("https://oss.sonatype.org/content/groups/public/") }
     maven { url = uri("https://repo.maven.apache.org/maven2/") }
 }
 
 dependencies {
-    implementation("me.txmc:protocolapi:1.1-SNAPSHOT_1.12.2")
+    implementation("me.txmc:protocolapi:1.2-SNAPSHOT")
     compileOnly("com.destroystokyo.paper:paper-jar:1.12.2-R0.1-SNAPSHOT")
     compileOnly("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.projectlombok:lombok:1.18.22")
+    compileOnly("com.comphenix.protocol:ProtocolLib:4.7.0");
 }
 
 tasks.shadowJar {
