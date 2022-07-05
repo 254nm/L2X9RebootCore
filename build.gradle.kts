c9fe0567f9e5a64bf186af53a148f87059c86c1a
Nate Steven Legault <67813185+254nm@users.noreply.github.com>
2022-07-05 13:15:57 -0400

    Remove protocollib shit

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,8 @@ plugins {
 
 repositories {
     mavenLocal()
-    maven { url = uri("https://repo.dmulloy2.net/repository/public/") } //This is only so I can patch chunkban on servers that use protocol lib
+    mavenCentral()
     maven { url = uri("https://repo.txmc.me/releases") }
-    maven { url = uri("https://oss.sonatype.org/content/groups/public/") }
-    maven { url = uri("https://repo.maven.apache.org/maven2/") }
 }
 
 dependencies {
@@ -16,7 +14,6 @@ dependencies {
     compileOnly("com.destroystokyo.paper:paper-jar:1.12.2-R0.1-SNAPSHOT")
     compileOnly("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.projectlombok:lombok:1.18.22")
-    compileOnly("com.comphenix.protocol:ProtocolLib:4.7.0");
 }
 
 tasks.shadowJar {
