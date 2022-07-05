450485780fd2585a49de5be071b50a1997fa3f74
Nate Steven Legault <67813185+254nm@users.noreply.github.com>
2022-07-05 13:13:00 -0400

    Rewrite chat message checking

--- src/main/java/me/l2x9/core/chat/ChatManager.java
+++ src/main/java/me/l2x9/core/chat/ChatManager.java
@@ -5,12 +5,14 @@ import me.l2x9.core.chat.listeners.ChatListener;
 import me.l2x9.core.chat.listeners.JoinLeaveListener;
 import me.l2x9.core.L2X9RebootCore;
 import me.l2x9.core.Manager;
+import me.l2x9.core.util.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
-import java.io.File;
+import java.io.*;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.UUID;
 
 public class ChatManager extends Manager {
@@ -35,10 +37,12 @@ public class ChatManager extends Manager {
     public void init(L2X9RebootCore plugin) {
         File dataFolder = getDataFolder();
         if (!dataFolder.exists()) dataFolder.mkdir();
+        File tldFile = new File(dataFolder, "tlds.txt");
+        if (!tldFile.exists()) Utils.unpackResource("tlds.txt", tldFile);
         ignoresFolder = new File(dataFolder, "IgnoreLists");
         if (!ignoresFolder.exists()) ignoresFolder.mkdir();
         config = plugin.getModuleConfig(this);
-        plugin.registerListener(new ChatListener(this));
+        plugin.registerListener(new ChatListener(this, parseTLDS(tldFile)));
         plugin.registerListener(new JoinLeaveListener(this));
         plugin.registerCommand ("ignore",new IgnoreCommand(this));
         plugin.getCommand("msg").setExecutor(new MessageCommand(this));
@@ -47,6 +51,20 @@ public class ChatManager extends Manager {
         plugin.registerCommand("unignore",new UnIgnoreCommand(this));
         if (!Bukkit.getOnlinePlayers().isEmpty()) Bukkit.getOnlinePlayers().forEach(this::registerPlayer);
 
+    }
+
+    private HashSet<String> parseTLDS(File tldFile) {
+        try {
+            HashSet<String> buf = new HashSet<>();
+            BufferedReader reader = new BufferedReader(new FileReader(tldFile));
+            reader.lines().filter(l -> !l.startsWith("#")).forEach(s -> buf.add(s.toLowerCase()));
+            reader.close();
+            return buf;
+        } catch (Throwable t) {
+            Utils.log("&cFailed to parse the TLD file please see the stacktrace below for more info!");
+            t.printStackTrace();
+            return null;
+        }
     }
 
     @Override
--- src/main/java/me/l2x9/core/chat/listeners/ChatListener.java
+++ src/main/java/me/l2x9/core/chat/listeners/ChatListener.java
@@ -1,8 +1,8 @@
 package me.l2x9.core.chat.listeners;
 
-import me.l2x9.core.util.Cooldown;
 import me.l2x9.core.chat.ChatInfo;
 import me.l2x9.core.chat.ChatManager;
+import me.l2x9.core.util.Cooldown;
 import me.l2x9.core.util.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -11,48 +11,71 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ChatListener implements Listener {
     private final ChatManager manager;
     private final Cooldown cooldown;
+    private final HashSet<String> tlds;
 
 
-    public ChatListener(ChatManager manager) {
+    public ChatListener(ChatManager manager, HashSet<String> tlds) {
         this.manager = manager;
+        this.tlds = tlds;
         cooldown = new Cooldown();
     }
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
         event.setCancelled(true);
-        boolean blocked = false;
         Player player = event.getPlayer();
+        String ogMessage = event.getMessage();
         if (cooldown.checkCooldown(player)) {
             cooldown.setCooldown(player, manager.getConfig().getInt("Cooldown"));
-        } else {
-            blocked = true;
+        } else return;
+        if (blockedCheck(ogMessage)) {
+            player.sendMessage("<" + player.getDisplayName() + "> " + ogMessage);
+            Utils.log("&3Prevented&r&a %s&r&3 from sending a message that has banned words", player.getName());
+            return;
         }
-        if (!blocked) {
-            List<String> list = manager.getConfig().getStringList("Blocked");
-            for (String word : list) {
-                if (event.getMessage().toLowerCase().contains(word)) {
-                    blocked = true;
-                    player.sendMessage("<" + player.getDisplayName()+ "> " + event.getMessage());
-                    Utils.log("&3Prevented&r&a " + player.getName() + "&r&3 from advertising");
-                    break;
-                }
-            }
+        if (domainCheck(ogMessage)) {
+            player.sendMessage("<" + player.getDisplayName() + "> " + ogMessage);
+            Utils.log("&3Prevented player&r&a %s&r&3 from sending a link / server ip", player.getName());
+            return;
         }
-        if (blocked) return;
-        Player chatter = event.getPlayer();
-        String message = (event.getMessage().startsWith(">")) ? "<" + chatter.getDisplayName() + "> " + ChatColor.GREEN + event.getMessage() : "<" + chatter.getDisplayName() + "> " + event.getMessage();
+        String message = (ogMessage.startsWith(">")) ? "<" + player.getDisplayName() + "> " + ChatColor.GREEN + ogMessage : "<" + player.getDisplayName() + "> " + ogMessage;
         Bukkit.getLogger().info(message);
         for (Player online : Bukkit.getOnlinePlayers()) {
             ChatInfo info = manager.getInfo(online);
             if (info == null) continue;
-            if (info.isIgnoring(chatter.getUniqueId()) || info.isToggledChat()) continue;
+            if (info.isIgnoring(player.getUniqueId()) || info.isToggledChat()) continue;
             online.sendMessage(message);
         }
+    }
+
+    private boolean domainCheck(String message) {
+        message = message.toLowerCase().replace("dot", ".").replace("d0t", ".");
+        if (message.indexOf('.') == -1) return false;
+        String[] split = message.trim().split("\\.");
+        if (split.length == 2) {
+            String possibleTLD = split[1];
+            if (possibleTLD.contains("/")) possibleTLD = possibleTLD.substring(0, possibleTLD.indexOf("/"));
+            return tlds.contains(possibleTLD);
+        } else {
+            for (String word : split) {
+                if (word.contains("/")) word = word.substring(0, word.indexOf("/"));
+                if (tlds.contains(word)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean blockedCheck(String message) {
+        List<String> blocked = manager.getConfig().getStringList("Blocked");
+        for (String blockedWord : blocked) {
+            if (message.toLowerCase().contains(blockedWord)) return true;
+        }
+        return false;
     }
 }
