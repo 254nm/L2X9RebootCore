c5c280bc898c3dc234318a769f2bd551226da10a
Mason Redacted <67813185+254nm@users.noreply.github.com>
2022-10-21 13:10:04 -0400

    Fix help formatting

--- src/main/java/me/l2x9/core/command/commands/BaseCmd.java
+++ src/main/java/me/l2x9/core/command/commands/BaseCmd.java
@@ -2,22 +2,12 @@ package me.l2x9.core.command.commands;
 
 import me.l2x9.core.command.BaseCommand;
 import me.l2x9.core.command.CommandManager;
-import me.l2x9.core.util.Utils;
 import org.bukkit.command.CommandSender;
 
 public class BaseCmd extends BaseCommand {
 
     public BaseCmd() {
-        super(
-                "aef",
-                "/aef reload | version | help",
-                "l2x9core.command.aef",
-                "Base command of the plugin",
-                new String[]{
-                        "reload::Reload the config file",
-                        "version::Show the version of the plugin",
-                        "help::Shows the help for the plugin"
-                });
+        super("aef", "/aef reload | version | help", "l2x9core.command.aef", "Base command of the plugin", new String[]{"reload::Reload the config file", "version::Show the version of the plugin", "help::Shows the help for the plugin"});
     }
 
     @Override
@@ -32,29 +22,28 @@ public class BaseCmd extends BaseCommand {
                     sendMessage(sender, PREFIX + "&6Version &r&c" + plugin.getDescription().getVersion());
                     break;
                 case "help":
-                    sendMessage(sender, PREFIX + "&1---&r " + PREFIX + "&6Help &r&1---");
-                    sendMessage(sender, PREFIX);
+                    sendMessage(sender, "&1---&r " + PREFIX + " &6Help &r&1---");
                     CommandManager.getInstance().getCommandHandler().getCommands().forEach(command -> {
                         String helpMsg = "&1---&r&3&l /" + command.getName() + "&r&6 Help &r&1---";
-                        sendMessage(sender, PREFIX + helpMsg);
-                        sendMessage(sender, PREFIX + "&3Description: " + command.getDescription());
+                        sendMessage(sender, helpMsg);
+                        sendMessage(sender, "&3Description: " + command.getDescription());
                         if (command.getSubCommands() != null) {
                             if (command.getSubCommands().length > 0) {
-                                sendMessage(sender, PREFIX + helpMsg.replace("Help", "Subcommands"));
+//                                sendMessage(sender, helpMsg.replace("Help", "Subcommands"));
                                 for (String subCommand : command.getSubCommands()) {
                                     String[] split = subCommand.split("::");
                                     if (split.length > 0) {
-                                        sendMessage(sender, PREFIX + "&6 /" + command.getName() + " " + split[0] + " |&r&e " + split[1]);
-                                    } else sendMessage(sender, PREFIX + "&6 /" + command.getName() + " " + subCommand);
+                                        sendMessage(sender, "&6/" + command.getName() + " " + split[0] + " |&r&e " + split[1]);
+                                    } else sendMessage(sender, "&6/" + command.getName() + " " + subCommand);
                                 }
-                                sendMessage(sender, PREFIX + "&1--------------------");
+                                sendMessage(sender, "&1--------------------");
                             }
                         }
-                        sendMessage(sender, PREFIX + "");
+                        sendMessage(sender, "");
                     });
                     break;
                 default:
-                    Utils.sendMessage(sender, String.format("&cUnknown command&r&3 %s&r", args[0]));
+                    sendMessage(sender, "&cUnknown command&r&3 %s&r", args[0]);
                     break;
             }
         } else {
