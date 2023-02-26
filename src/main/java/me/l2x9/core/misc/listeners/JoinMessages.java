90d14e03fe938e6a840a228b3ef6cb1720c3b4c3
Mason Redacted <67813185+254nm@users.noreply.github.com>
2023-02-26 17:19:21 -0500

    feat(misc): Add joined for first time & kick indicator

--- src/main/java/me/l2x9/core/misc/listeners/JoinMessages.java
+++ src/main/java/me/l2x9/core/misc/listeners/JoinMessages.java
@@ -1,37 +1,39 @@
 package me.l2x9.core.misc.listeners;
 
-import me.l2x9.core.L2X9RebootCore;
 import me.l2x9.core.util.Utils;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.Arrays;
 
 public class JoinMessages implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
         event.setJoinMessage(null);
-        Bukkit.getOnlinePlayers().forEach(p -> Utils.sendLocalizedMessage(p, "join_message", false, player.getName()));
+        Player player = event.getPlayer();
+        String key = (!player.hasPlayedBefore()) ? "player_join_first_time" : "join_message";
+        Bukkit.getOnlinePlayers().forEach(p -> Utils.sendLocalizedMessage(p, key, false, player.getName()));
     }
 
     @EventHandler
     public void onLeave(PlayerQuitEvent event) {
-        Player player = event.getPlayer();
         event.setQuitMessage(null);
+        Player player = event.getPlayer();
+        if (Arrays.stream(Thread.currentThread().getStackTrace()).map(StackTraceElement::toString).anyMatch(e -> e.contains("PlayerConnection.disconnect") || e.contains("Utils.kick"))) return;
         Bukkit.getOnlinePlayers().forEach(p -> Utils.sendLocalizedMessage(p, "leave_message", false, player.getName()));
+    }
 
-        if (player.isOp() && !L2X9RebootCore.getInstance().isDebug()) {
-            player.setOp(false);
-        }
-
-        if (!player.getGameMode().equals(GameMode.SURVIVAL) && !L2X9RebootCore.getInstance().isDebug()) {
-            player.setGameMode(GameMode.SURVIVAL);
-        }
+    @EventHandler
+    public void onKick(PlayerKickEvent event) {
+        event.setLeaveMessage(null);
+        Player player = event.getPlayer();
+        String reason = event.getReason();
+        Bukkit.getOnlinePlayers().forEach(p -> Utils.sendLocalizedMessage(p, "kick_message", false, player.getName(), reason));
     }
 }
